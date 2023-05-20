2d7873f4257c7b1dbe06d60c6f3eca38ba46ab6f
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2023-05-20 17:02:29 -0300

    fluxo de rotas autenticadas concluida

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,22 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true,
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
+app.register(memoriesRoutes)
 
 app
   .listen({
