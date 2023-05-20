1de89890f7b3ba773aafb6fc5f7c476a7a29359b
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2023-05-20 16:55:02 -0300

    home page finished

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed">
+      Feito com 💜 no NLW da{' '}
+      <a
+        className="underline hover:text-gray-50"
+        href="https://rocketseat.com.br"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
