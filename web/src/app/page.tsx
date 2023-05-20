b859546c22c95e11ceed5c4b5838ec349ea80323
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2023-05-20 18:48:14 -0300

    Token em Cookie criado, retornando Avatar image e nome

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,14 @@
+import { cookies } from 'next//headers'
+
 import { Copyright } from '@/components/Copyright'
 import { Hero } from '@/components/Hero'
 import { CreateAccount } from '@/components/CreateAccount'
 import { EmptyMemories } from '@/components/EmptyMemories'
+import { Profile } from '@/components/Profile'
 
 export default function Home() {
+  const isAuthenticate = cookies().has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16 ">
@@ -12,7 +17,7 @@ export default function Home() {
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        <CreateAccount />
+        {isAuthenticate ? <Profile /> : <CreateAccount />}
         <Hero />
         <Copyright />
       </div>
